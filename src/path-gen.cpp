a6a4216307fa9742f7695b8743ce7ddad16cfed7
oven02 <ows.cassidy@gmail.com>
2025-12-01 15:13:34 -0600

    added 2 and 4th degree curves

--- src/path-gen.cpp
+++ src/path-gen.cpp
@@ -4,9 +4,14 @@ namespace gen{
 std::vector<float> spline(enum degree deg, std::vector<std::pair<float, float>> pts, int samples){
     std::vector<float> spline
     if (deg == TWO){
+        float x0 = pts[0][0];
+        float y0 = pts[0][1];
+        float x1 = pts[1][0];
+        float y1 = pts[1][1];
         for(float t = 0, t <= 1, t += 1.0f/samples){
-
+            spline.append((1-t)*x0+t*x1, (1-t)*x0+t*x1);
         }
+    return spline;
 
     }else if (deg == THREE){
         float x0 = pts[0][0];
@@ -21,9 +26,19 @@ std::vector<float> spline(enum degree deg, std::vector<std::pair<float, float>> 
         }
         return spline;
     }else if (deg == FOUR){
+        float x0 = pts[0][0];
+        float y0 = pts[0][1];
+        float x1 = pts[1][0];
+        float y1 = pts[1][1];
+        float x2 = pts[2][0];
+        float y2 = pts[2][1];
+        float x3 = pts[2][0];
+        float y3 = pts[2][1];
         for(float i = 0, i <= 1, i += 1.0f/samples){
+            spline.append(((1-t)**3)*x0 + 3*((1-t)**2)*t*x1 + 3*((1-t))*(t**2)*x2 + (t**3)*x3, ((1-t)**3)*y0 + 3*((1-t)**2)*t*y1 + 3*((1-t))*(t**2)*y2 + (t**3)*y3);
             
         }
+        return spline;
     }else if (deg == FIVE){
         for(float i = 0, i <= 1, i += 1.0f/samples){
             
