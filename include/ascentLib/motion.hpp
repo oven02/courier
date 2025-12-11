329f545c1c55e56ce8839a0b74c304953802ccf7
oven02 <ows.cassidy@gmail.com>
2025-12-11 14:05:29 -0600

    updated hpp file

--- include/ascentLib/motion.hpp
+++ include/ascentLib/motion.hpp
@@ -3,7 +3,10 @@
 #include <vector>
 #include <main.h>
 
-std::vector<double> toPointStep(float sigX,float sigY);
+std::vector<double> toPointStep(float sigX,float sigY, std::vector<float> pos);
 void toPoint(float tarX, float tarY, float exit);
 std::vector<double> getOuts();
+
+std::vector<double> toPointStep(float sigX,float sigY, std::vector<float> pos);
+void toPoint(float tarT, float exit);
 
